41773ae77a618da4315cd014c49afc49283f3b7c
ZephonMakale <mzephon@gmail.com>
2021-01-11 10:09:50 +0300

    Modified the paginator

--- src/posts/views.py
+++ src/posts/views.py
@@ -21,7 +21,7 @@ def index(request):
 
 def blog(request):
     post_list = Post.objects.all()
-    paginator = Paginator(post_list, 1)
+    paginator = Paginator(post_list, 4)
     page_request_var = 'page' 
     page = request.GET.get(page_request_var)
     try:
@@ -32,7 +32,7 @@ def blog(request):
         paginated_queryset = paginator.page(paginator.num_pages)
 
     context = {
-        'post_list': paginated_queryset,
+        'queryset': paginated_queryset,
         'page_request_var': page_request_var
 
     }
