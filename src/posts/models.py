43dbf39e5c33c504d65cec282319930979f5c9c1
ZephonMakale <mzephon@gmail.com>
2021-01-11 16:46:29 +0300

    Added the comment property

--- src/posts/models.py
+++ src/posts/models.py
@@ -48,12 +48,15 @@ class Post(models.Model):
             'id': self.id
         })
 
+    @property
+    def comments(self):
+        return self.comments.all()
 
 class Comment(models.Model):
     user = models.OneToOneField(User, on_delete = models.CASCADE)
     timestamp = models.DateTimeField(auto_now_add = True)
     content = models.TextField()
-    post = models.ForeignKey(Post, on_delete = models.CASCADE)
+    post = models.ForeignKey(Post, related_name = 'comments', on_delete = models.CASCADE)
     
     def __str__(self):
         return self.user.username
